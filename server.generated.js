8c53434855097792584fc8d52f3116a2a61a61af
Grace951 <learningg@gmail.com>
2017-03-06 10:35:31 +0800

    GA local for google pagespeed insight

--- server.generated.js
+++ server.generated.js
@@ -21354,15 +21354,18 @@ var Root = function (_React$Component) {
 		document.getElementById("search").appendChild(gcsecc);
 
 		//GA
-		// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		// (i[r].q=i[r].q||[]).push(arguments);},i[r].l=1*new Date();a=s.createElement(o),
-		// m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);
-		// })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-		// ga('create', 'UA-50969260-2', 'auto');
-		// ga('send', 'pageview');
-		this.loadScript("https://cdn.jsdelivr.net/ga-lite/latest/ga-lite.min.js").then(function () {
-			var galite = galite || {};galite.UA = 'UA-50969260-2';
-		});
+		(function (i, s, o, g, r, a, m) {
+			i['GoogleAnalyticsObject'] = r;i[r] = i[r] || function () {
+				(i[r].q = i[r].q || []).push(arguments);
+			}, i[r].l = 1 * new Date();a = s.createElement(o), m = s.getElementsByTagName(o)[0];a.async = 1;a.src = g;m.parentNode.insertBefore(a, m);
+		})(window, document, 'script', '/local-ga.js', 'ga');
+		ga('create', 'UA-50969260-2', 'auto');
+		ga('send', 'pageview');
+		// this.loadScript("https://cdn.jsdelivr.net/ga-lite/latest/ga-lite.min.js")
+		// .then(()=>{
+		// 	let galite = galite || {}; galite.UA = 'UA-50969260-2';
+		// });
+
 
 		//Google Web fonts
 		this.loadScript("https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js").then(function () {
