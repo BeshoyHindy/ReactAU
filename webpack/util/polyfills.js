385deae589a57b5275ed554169be639fd6529df1
ChingChing Yeh <learningg@gmail.com>
2017-10-07 11:14:00 +0800

    for IE

--- webpack/util/polyfills.js
+++ webpack/util/polyfills.js
@@ -13,7 +13,31 @@ require('es6-promise').polyfill();
 
 // Object.assign() is commonly used with React.
 // It will use the native implementation if it's present and isn't buggy.
-Object.assign = require('object-assign');
+
+if (typeof Object.assign != 'function') {
+	Object.assign = function (target, varArgs) { // .length of function is 2
+	'use strict';
+	if (target == null) { // TypeError if undefined or null
+		throw new TypeError('Cannot convert undefined or null to object');
+	}
+
+	var to = Object(target);
+
+	for (var index = 1; index < arguments.length; index++) {
+		var nextSource = arguments[index];
+
+		if (nextSource != null) { // Skip over if undefined or null
+		for (var nextKey in nextSource) {
+			// Avoid bugs when hasOwnProperty is shadowed
+			if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+			to[nextKey] = nextSource[nextKey];
+			}
+		}
+		}
+	}
+	return to;
+	};
+}
 
 
 
